aaa24e7dd052f3f54f31482d6ed04ce7a42cf119
lasseveenliese <129764721+lasseveenliese@users.noreply.github.com>
2023-11-19 00:42:05 +0100

    Add files via upload

---
+++ pages/edit/_name.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="section">
+    <tree-view v-model="entry" disable-name-edit @submit="save"> </tree-view>
+    <b-field grouped>
+      <div class="control is-expanded">
+        <b-button @click="cancel"> Abbrechen </b-button>
+      </div>
+      <div class="control">
+        <b-button
+          tag="input"
+          native-type="submit"
+          type="is-success"
+          value="Speichern"
+          form="tree-view"
+        >
+        </b-button>
+      </div>
+    </b-field>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import TreeView from '@/components/treeView.vue';
+
+@Component({
+  components: {
+    TreeView,
+  },
+})
+export default class EditView extends Vue {
+  entry: null | any = null;
+
+  async created() {
+    this.entry = await this.$axios.$get(`entries/${this.$route.params.name}`);
+  }
+
+  cancel() {
+    this.$router.push({ name: 'index' });
+  }
+
+  save() {
+    if (this.entry) {
+      this.$axios.$put(`entries/${this.entry.name}`, this.entry);
+      this.$router.push({ name: 'index' });
+    }
+  }
+}
+</script>
